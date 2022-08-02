dc680f65b79ec48380741bfb74d6eff859b59e81
DudeCalledZee <ineedtothink@gmail.com>
2022-08-02 15:56:49 +0100

    gameover

--- KafkaCourse/src/main/java/org/course/simplekafka/kafka/twitter/TwitterProducer.java
+++ KafkaCourse/src/main/java/org/course/simplekafka/kafka/twitter/TwitterProducer.java
@@ -1,21 +1,83 @@
 package org.course.simplekafka.kafka.twitter;
 
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public class TwitterProducer {
-
-    public void run(){
-
-    };
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+    String consumerkey = "";
+    String consumersecret = "";
+    String token = "";
+    String tokenSecret = "";
 
     public static void main(String[] args) {
         new TwitterProducer().run();
+        System.out.println("hi");
     }
+
+    public void run() {
+        logger.info("SETUP");
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(1000);
 
         // create twitter client
-    public void createTwitterClient() {
+        Client client = createTwitterClient(msgQueue);
+        client.connect();
 
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null) {
+                logger.info(msg);
+            }
+        }
+        logger.info("END OF APPLICATION");
     }
 
-        //create a kafka producer
+    public Client createTwitterClient(BlockingQueue<String> msgQueue) {
 
-        //loop to send tweets to kafka
+        // Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+
+        List<String> terms = Lists.newArrayList("bitcoin");
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+
+        Authentication hosebirdAuth = new OAuth1(consumerkey, consumersecret, token, tokenSecret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        return builder.build();
+    }
+
+    //create a kafka producer
+
+    //loop to send tweets to kafka
+
 }
